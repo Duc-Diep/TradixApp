3b1686d717a3eee53a8e392e3b56804c2143eff9
Đức Điệp <=>
2021-08-31 22:55:16 +0700

    commit 31/08

--- app/src/main/java/com/example/tradixapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/tradixapp/activities/MainActivity.kt
@@ -1,37 +1,57 @@
 package com.example.tradixapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.AdapterView
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.tradixapp.R
-import com.example.tradixapp.fragments.CoinFragment
-import com.example.tradixapp.fragments.CustomeFragment
-import com.example.tradixapp.fragments.HomeFragment
-import com.example.tradixapp.fragments.NewsFragment
-import com.google.android.material.navigation.NavigationBarView
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 fun AppCompatActivity.hideActionBar() {
     supportActionBar?.hide()
 }
 class MainActivity : AppCompatActivity() {
+    lateinit var navController:NavController
+    private val navBuilder = NavOptions.Builder()
+    lateinit var navHostFragment:NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         hideActionBar()
-        supportFragmentManager.beginTransaction().replace(R.id.frame_main,HomeFragment()).commit()
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+        bottom_navi.setupWithNavController(navController)
         bottom_navi.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.home -> getFragment(HomeFragment())
-                R.id.news -> getFragment(NewsFragment())
-                R.id.chart -> getFragment(CoinFragment())
-                R.id.customer -> getFragment(CustomeFragment())
+                R.id.fragment_home -> addToBackStack(R.id.fragment_home)
+                R.id.fragment_news -> addToBackStack(R.id.fragment_news)
+                R.id.fragment_coin -> addToBackStack(R.id.fragment_coin)
+                R.id.fragment_customer -> addToBackStack(R.id.fragment_customer)
             }
             true
         }
     }
-    private fun getFragment(fragment:Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.frame_main,fragment).addToBackStack(null).commit()
+    private fun addToBackStack(id:Int){
+        val navOptions = navBuilder.setPopUpTo(id, false).build()
+        navController.navigate(id,null,navOptions)
+    }
+
+    override fun onBackPressed() {
+        val navHostManager = navHostFragment.childFragmentManager
+        if (navHostManager.backStackEntryCount==0){
+            var dialog = AlertDialog.Builder(this)
+                .setTitle("Confirm")
+                .setMessage("Do you want to close this app?")
+                .setNegativeButton("No") { _, _ -> }
+                .setPositiveButton("Yes") { _, _ -> finish() }
+                .create()
+            dialog.show()
+        }else{
+            super.onBackPressed()
+        }
     }
 }
